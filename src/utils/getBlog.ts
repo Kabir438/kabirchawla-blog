e37b93bc689f61f61aa126b879771f2e4a07ffa1
Kabir Chawla <kabir.438.2025@doonschool.com>
2024-10-23 22:24:55 +0530

    fixed

--- src/utils/getBlog.ts
+++ src/utils/getBlog.ts
@@ -8,12 +8,13 @@ import { blogsSchema } from "./getBlogs";
 const commentsSchema = z.array(z.object({
     message: z.string(),
     name: z.string(),
-    commented_at: z.string().refine(i =>!Number.isNaN(Date.parse(i)))
+    commented_at: z.string().refine(i => !Number.isNaN(Date.parse(i)))
 }))
 
 export default async function getBlog(slug: string) {
     const query = `*[_type == "post" && slug.current == ${slug}]{...,author->{...},categories[]->{...},comments[]->{...}}`;
     const result = await client.fetch(query, {}, { cache: 'no-cache' });
+    console.log("result", result, slug)
     const parsedResult = await blogsSchema.parseAsync(result);
     const firebaseRes = await db
         .collection("posts")
@@ -33,13 +34,14 @@ export default async function getBlog(slug: string) {
         "viewCount" in firebaseData &&
         typeof firebaseData.viewCount === "number"
     ) ? firebaseData.viewCount : 0;
+    const post = parsedResult.find(p => { return p.href.current === slug }) || parsedResult[0];
     const comments = await commentsSchema.parseAsync((await db
         .collection('posts')
-        .doc(parsedResult[0]._id)
+        .doc(post._id)
         .collection('comments')
         .get()).docs.map(doc => doc.data()));
-    return { 
-        ...parsedResult[0],
+    return {
+        ...post,
         actualViews: viewCount,
         comments
     } satisfies typeof parsedResult[number];
